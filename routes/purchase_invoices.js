7eeaa36d6efc0ce0ca57c969af72de0ac3d5db47
FabioLopes021 <a25999@alunos.ipca.pt>
2024-05-13 23:31:57 +0100

    purchase invoice

---
+++ routes/purchase_invoices.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+const purchaseInvoiceController = require("../controllers/purchase_invoices");
+const login = require("../middleware/login");
+
+// list All purchases invoices
+router.get("/purchaseInvoices", purchaseInvoiceController.getAllPurchases);
+
+// list certain purchases invoices
+router.get("/purchaseInvoices/:id", purchaseInvoiceController.getPurchase);
+
+// list All purchases invoices related to a museum
+router.get("/purchaseInvoices/museum/:id", purchaseInvoiceController.getPurchasesByMuseum);
+
+// Add purchase invoice
+router.post("/purchaseInvoices/add", login.required, purchaseInvoiceController.addPurchase);
+
+// Edit purchase invoice
+router.put("/purchaseInvoices/edit/:id", login.required, purchaseInvoiceController.editPurchase);
+
+// Remove purchase invoice
+router.delete("/purchaseInvoices/remove/:id", login.required, purchaseInvoiceController.removePurchase);
+
+module.exports = router;
